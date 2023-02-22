d965f1dfdcd5c1e04de2babb27a064a29fbe9eb9
Emil Lantz <emil.lantz@gmail.com>
2023-02-22 18:58:28 +0100

    webserver runs on pi3

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use actix_web::{web, App, HttpServer};
+use actix_cors::Cors;
+
+mod routes;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Starting server");
+    HttpServer::new(|| {
+        App::new()
+            .service(routes::test::test)
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
+}
